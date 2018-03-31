01e9ca27bc20bd8030e9b7fa2b702e95cb0b123e
Chase <chaseit12@gmail.com>
2018-03-31 16:24:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file!
 
 
-function distanceFromHqInBlocks(){
-  
+function distanceFromHqInBlocks(distance){
+  return distance * 264
 }
 
 function distanceFromHqInFeet(){
